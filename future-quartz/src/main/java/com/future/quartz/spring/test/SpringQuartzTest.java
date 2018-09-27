8d3f597571f371bc39c92b6d17bac2f28e694465
tianyu <18610639412@163.com>
2018-09-27 17:53:34 +0800

    quartz

--- future-quartz/src/main/java/com/future/quartz/spring/test/SpringQuartzTest.java
+++ future-quartz/src/main/java/com/future/quartz/spring/test/SpringQuartzTest.java
@@ -16,22 +16,29 @@ public class SpringQuartzTest {
         //Scheduler scheduler= (Scheduler) context.getBean("scheduler");
         Scheduler jdbcscheduler= (Scheduler) context.getBean("jdbcscheduler");
         try {
-            //scheduler.start();
-            JobDetail jobDetail = JobBuilder.newJob(FirstJob.class)
-                    .withIdentity("job1_1","jgroup1").build();
-            CronScheduleBuilder builder=CronScheduleBuilder.cronSchedule("0/2 * * * * ?");
-            Trigger trigger=TriggerBuilder.newTrigger()
-                    .withIdentity("trigger1_1","tgroup1")
-                    .startNow().withSchedule(builder)
-                    .build();
             Scheduler scheduler= StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            scheduler.scheduleJob(jobDetail,trigger);
-            Thread.sleep(10000);
+            boolean  hasJob= scheduler.checkExists(JobKey.jobKey("job1_1","jgroup1"));
+            JobDetail jobDetail= scheduler.getJobDetail(JobKey.jobKey("job1_1","jgroup1"));
+            if(hasJob==false) {
+                 jobDetail = JobBuilder.newJob(FirstJob.class)
+                        .withIdentity("job1_1", "jgroup1").build();
+            }
+            boolean  hasTrigger= scheduler.checkExists(TriggerKey.triggerKey("trigger1_1","tgroup1"));
+            Trigger trigger=scheduler.getTrigger(TriggerKey.triggerKey("trigger1_1","tgroup1"));
+            if(hasTrigger==false) {
+                CronScheduleBuilder builder = CronScheduleBuilder.cronSchedule("0/2 * * * * ?");
+                trigger = TriggerBuilder.newTrigger()
+                        .withIdentity("trigger1_1", "tgroup1")
+                        .startNow().withSchedule(builder)
+                        .build();
+            }
+            if(!hasJob &&!hasTrigger){
+                scheduler.scheduleJob(jobDetail,trigger);
+            }
+
             scheduler.shutdown();
         } catch (SchedulerException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
